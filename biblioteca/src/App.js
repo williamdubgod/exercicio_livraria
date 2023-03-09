89b9a0d4cbb196e4c10c33eed79919becc6413c5
William Vulcano <99343816+williamdubgod@users.noreply.github.com>
2023-03-08 22:57:14 -0300

    Add files via upload

---
+++ biblioteca/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Nav from './componentes/Nav';
+import Home from './componentes/Home';
+
+
+function App() {
+  return (
+    <>
+    <Nav />
+    <Home /> 
+    </>
+  );
+}
+
+export default App;
